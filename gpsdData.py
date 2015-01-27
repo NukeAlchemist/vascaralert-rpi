96dfe09d609751339970fcc595c1cb75826ec88d
NukeAlchemist <NukeAlchemist@users.noreply.github.com>
2015-01-27 10:44:44 -0600

    Small changes before public release.

--- gpsdData.py
+++ gpsdData.py
@@ -3,7 +3,6 @@
 import os
 from gps import *
 from time import *
-import time
 import threading
 import urllib2
 import json
@@ -11,29 +10,34 @@ from math import radians, degrees, cos, sin, asin, sqrt, atan2
 
 gpsd = None #seting the global variable
 dump1090url = 'http://127.0.0.1:8080'
-distanceLimit = 5
-speedthresh = 0
-altthresh = 2500
-timethresh = 25
+
+## Configuration Variables ##
+distanceLimit = 8	# Maximum distance (mi) to potential aerial VASCAR threats. Only works if lat/lon received.
+speedthresh = 0		# Vehicle speed (MPH) threshold for activating aerial VASCAR scanning.
+altthresh = 2500	# Maximum rel. altitude (ft) for potential aerial VASCAR threats. 
+timethresh = 30		# Maximum time (s) since receipt of last beacon for potential aerial VASCAR threats.
+msgthresh = 50		# Minimum received messages to consider aerial VASCAR threats. Reduces falses.
+
+
 alert = 0
 oldalert = 0
 
 class GpsPoller(threading.Thread):
 	def __init__(self):
 		threading.Thread.__init__(self)
-		global gpsd #bring it in scope
-		gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
+		global gpsd 			#bring it in scope
+		gpsd = gps(mode=WATCH_ENABLE) 	#starting the stream of info
 		self.current_value = None
-		self.running = True #setting the thread running to true
+		self.running = True 		#setting the thread running to true
 
 	def run(self):
 		global gpsd
 		while gpsp.running:
-			gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+			gpsd.next() 		#this will continue to loop and grab EACH set of gpsd info to clear the buffer
 
 def haversine(lon1, lat1, lon2, lat2):
-#	Calculate the great circle distance between two points
-#	on the earth (specified in decimal degrees)
+	# Calculate the great circle distance between two points
+	# on the earth (specified in decimal degrees)
 
 	# convert decimal degrees to radians
 	lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
@@ -54,9 +58,9 @@ if __name__ == '__main__':
 			oldalert = alert
 			alert = 0
 
-			#It may take a second or two to get good data
-			#print gpsd.fix.latitude,', ',gpsd.fix.longitude,' Time: ',gpsd.utc
-			time.sleep(15)
+			# It may take a second or two to get good data
+			# print gpsd.fix.latitude,', ',gpsd.fix.longitude,' Time: ',gpsd.utc
+			sleep(15)
 
 			alt = gpsd.fix.altitude * 3.28084
 			speed = gpsd.fix.speed * 2.23694
@@ -67,34 +71,35 @@ if __name__ == '__main__':
 
 				s = urllib2.urlopen(url).read()
 				j = json.loads(s)
-		
+
 				for plane in j:
 					if plane['lon']:
 						dist = haversine(lon, lat, plane['lon'], plane['lat'])
-						plane['dist'] = int(dist)
+						plane['dist'] = round(dist,2)
 					else:
 						plane['dist'] = 0
-					plane['relalt'] = int(plane['altitude'] - alt)	
+					plane['relalt'] = round(plane['altitude'] - alt)	
 
 				sortedByDistance = sorted(j, key=lambda k: k['seen'])
 
-				for i in range(0,100):
+				for i in range(0,len(j)):
 					if len(sortedByDistance) > i:
 						p = sortedByDistance[i]
 						#analyze
-						if p['relalt'] > 0 and p['relalt'] < altthresh and p['seen'] < timethresh: 
+						if p['relalt'] > 0 and p['relalt'] < altthresh and p['seen'] < timethresh and p['messages'] > msgthresh: 
 							alert = 1
-							print "Plane at", p['relalt'], "ft. rel. altitude.", p['seen'], "(s) since beacon,", p['dist'], "miles away."
+							print "\nPlane at", p['relalt'], "ft. rel. altitude.", p['seen'], "(s) since beacon,", p['dist'], "miles away at", strftime("%H:%M:%S", localtime())
 						p = []
 				s = []
 				j = []
 
 			if alert == 1 and oldalert == 0:
-				print "\nALERT: New potential VASCAR threat.\n"
+				print "ALERT: New potential VASCAR threat.\n"
 			elif alert == 1 and oldalert == 1:
 				print "Potential VASCAR threat (old)."
 #			else:
 #				print "You're good!"
+
 	except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
 		print "\nKilling Thread..."
 		gpsp.running = False
