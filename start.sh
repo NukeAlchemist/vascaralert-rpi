751e012dff515f18c1e0cce31ffb51c9a999e01b
Evan L <NukeAlchemist@users.noreply.github.com>
2015-02-10 11:28:26 -0600

    Added "-n" tag to gpsd startup

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 # This script is necessary with certain GPS receivers to reliably
 # start the GPS daemon.
 
-killall gpsd; sleep 2; gpsd /dev/ttyUSB0 -F -b /var/run/gpsd.sock
+killall gpsd; sleep 2; gpsd /dev/ttyUSB0 -F -b -n /var/run/gpsd.sock
